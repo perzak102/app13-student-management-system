fd4dc24fc2032de2036171d8463f7c5c9cb54604
ubuntu <piotr.perz@wp.pl>
2024-10-07 23:47:08 +0200

    Day 47: Finished after refactor

--- main.py
+++ main.py
@@ -6,6 +6,13 @@ import sys
 import sqlite3
 
 
+class DatabaseConnection:
+    def __init__(self, database_file="database.db"):
+        self.database_file = database_file
+
+    def connect(self):
+        connection = sqlite3.connect(self.database_file)
+        return connection
 
 
 class MainWindow(QMainWindow):
@@ -18,7 +25,7 @@ class MainWindow(QMainWindow):
         help_menu_item = self.menuBar().addMenu("&Help")
         edit_menu_item = self.menuBar().addMenu("&Edit")
 
-        add_student_action = QAction(QIcon("icons/add.png"),"Add Student", self)
+        add_student_action = QAction(QIcon("icons/add.png"), "Add Student", self)
         add_student_action.triggered.connect(self.insert)
         file_menu_item.addAction(add_student_action)
 
@@ -26,7 +33,7 @@ class MainWindow(QMainWindow):
         help_menu_item.addAction(about_action)
         about_action.triggered.connect(self.about)
 
-        search_action = QAction(QIcon("icons/search.png"),"Search", self)
+        search_action = QAction(QIcon("icons/search.png"), "Search", self)
         search_action.triggered.connect(self.search)
         edit_menu_item.addAction(search_action)
 
@@ -50,7 +57,6 @@ class MainWindow(QMainWindow):
         # Detect a cell click
         self.table.cellClicked.connect(self.cell_clicked)
 
-
     def cell_clicked(self):
         edit_button = QPushButton("Edit Record")
         edit_button.clicked.connect(self.edit)
@@ -67,7 +73,7 @@ class MainWindow(QMainWindow):
         self.statusbar.addWidget(delete_button)
 
     def load_data(self):
-        connection = sqlite3.connect("database.db")
+        connection = DatabaseConnection().connect()
         result = connection.execute("SELECT * FROM students")
         self.table.setRowCount(0)
         for row_number, row_data in enumerate(result):
@@ -137,7 +143,7 @@ class EditDialog(QDialog):
         self.student_id = main_window.table.item(index, 0).text()
 
         # Add mobile widget
-        mobile = main_window.table.item(index, 3). text()
+        mobile = main_window.table.item(index, 3).text()
         self.mobile = QLineEdit(mobile)
         self.mobile.setPlaceholderText("Mobile")
         layout.addWidget(self.mobile)
@@ -150,13 +156,13 @@ class EditDialog(QDialog):
         self.setLayout(layout)
 
     def update_student(self):
-        connection = sqlite3.connect("database.db")
+        connection = DatabaseConnection().connect()
         cursor = connection.cursor()
         cursor.execute("UPDATE students SET name = ?, course = ?, mobile = ? WHERE id = ?",
-                    (self.student_name.text(),
-                     self.course_name.itemText(self.course_name.currentIndex()),
-                     self.mobile.text(),
-                     self.student_id))
+                       (self.student_name.text(),
+                        self.course_name.itemText(self.course_name.currentIndex()),
+                        self.mobile.text(),
+                        self.student_id))
         connection.commit()
         cursor.close()
         connection.close()
@@ -183,20 +189,19 @@ class DeleteDialog(QDialog):
         no.clicked.connect(self.abort)
 
     def delete_student(self):
-
         # Get selected row index and student id
         index = main_window.table.currentRow()
         student_id = main_window.table.item(index, 0).text()
 
-        connection = sqlite3.connect("database.db")
+        connection = DatabaseConnection().connect()
         cursor = connection.cursor()
-        cursor.execute("DELETE from students WHERE id = ?", (student_id, ))
+        cursor.execute("DELETE from students WHERE id = ?", (student_id,))
         connection.commit()
         cursor.close()
         connection.close()
         main_window.load_data()
 
-        self. close()
+        self.close()
 
         confirmation_widget = QMessageBox()
         confirmation_widget.setWindowTitle("Success")
@@ -210,6 +215,7 @@ class DeleteDialog(QDialog):
         confirmation_widget.exec()
 
         self.close()
+
 
 class InsertDialog(QDialog):
     def __init__(self):
@@ -247,7 +253,7 @@ class InsertDialog(QDialog):
         name = self.student_name.text()
         course = self.course_name.itemText(self.course_name.currentIndex())
         mobile = self.mobile.text()
-        connection = sqlite3.connect("database.db")
+        connection = DatabaseConnection().connect()
         cursor = connection.cursor()
         cursor.execute("INSERT INTO students (name, course, mobile) VALUES (?, ?, ?)",
                        (name, course, mobile))
@@ -255,6 +261,7 @@ class InsertDialog(QDialog):
         cursor.close()
         connection.close()
         main_window.load_data()
+
 
 class SearchDialog(QDialog):
     def __init__(self):
@@ -277,11 +284,9 @@ class SearchDialog(QDialog):
 
         self.setLayout(layout)
 
-
-
     def search(self):
         name = self.student_name.text()
-        connection = sqlite3.connect("database.db")
+        connection = DatabaseConnection().connect()
         cursor = connection.cursor()
         result = cursor.execute("SELECT * FROM students WHERE name = ?", (name,))
         rows = list(result)
@@ -293,7 +298,6 @@ class SearchDialog(QDialog):
 
         cursor.close()
         connection.close()
-
 
 
 app = QApplication(sys.argv)
